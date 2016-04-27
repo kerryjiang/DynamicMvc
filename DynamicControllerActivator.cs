ced0ada5f0c8ef3a7b700aefc0315d629f040c64
Kerry Jiang <kerry-jiang@hotmail.com>
2016-04-27 13:57:53 +0800

    removed useless code

--- DynamicControllerActivator.cs
+++ DynamicControllerActivator.cs
@@ -19,53 +19,5 @@ namespace DynamicMvc
         {
 
         }
-
-        /// <inheritdoc />
-        public virtual object Create(ControllerContext controllerContext)
-        {
-            if (controllerContext == null)
-            {
-                throw new ArgumentNullException(nameof(controllerContext));
-            }
-
-            if (controllerContext.ActionDescriptor == null)
-            {
-                throw new ArgumentException(Resources.FormatPropertyOfTypeCannotBeNull(
-                    nameof(ControllerContext.ActionDescriptor),
-                    nameof(ControllerContext)));
-            }
-
-            var controllerTypeInfo = controllerContext.ActionDescriptor.ControllerTypeInfo;
-
-            if (controllerTypeInfo == null)
-            {
-                throw new ArgumentException(Resources.FormatPropertyOfTypeCannotBeNull(
-                    nameof(controllerContext.ActionDescriptor.ControllerTypeInfo),
-                    nameof(ControllerContext.ActionDescriptor)));
-            }
-
-            var serviceProvider = controllerContext.HttpContext.RequestServices;
-            return _typeActivatorCache.CreateInstance<object>(serviceProvider, controllerTypeInfo.AsType());
-        }
-
-        /// <inheritdoc />
-        public virtual void Release(ControllerContext context, object controller)
-        {
-            if (context == null)
-            {
-                throw new ArgumentNullException(nameof(context));
-            }
-
-            if (controller == null)
-            {
-                throw new ArgumentNullException(nameof(controller));
-            }
-
-            var disposable = controller as IDisposable;
-            if (disposable != null)
-            {
-                disposable.Dispose();
-            }
-        }
     }
 }
